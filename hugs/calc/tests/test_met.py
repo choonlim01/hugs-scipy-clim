1a3f7d488b531a8ca7a3cce825d056e5b102ba10
Choon Lim <bosch.c.lim@daimler.com>
2018-07-10 09:25:11 -0700

    Integrating Flake 8 into CI

--- hugs/calc/tests/test_met.py
+++ hugs/calc/tests/test_met.py
@@ -1,16 +1,18 @@
 """Test the `met` module."""
 
-from hugs.calc import get_wind_dir, get_wind_speed, get_wind_components
-from hugs.calc import snell_angle 
+from hugs.calc import get_wind_components, get_wind_dir, get_wind_speed
+from hugs.calc import snell_angle
 
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
-import pytest 
+
+import pytest
+
 
 def test_speed():
     """Test calculating wind speed."""
-    u = np.array([4., 2.,0., 0.])
-    v = np.array([0.,2., 4., 0.])
+    u = np.array([4., 2., 0., 0.])
+    v = np.array([0., 2., 4., 0.])
 
     speed = get_wind_speed(u, v)
 
@@ -37,27 +39,27 @@ def test_dir():
 
     assert_array_almost_equal(true_dir, direc, 4)
 
+
 def test_wind_component():
-   """ Test the wind component"""
-   u,v = get_wind_components(100, 0) 
-   assert_almost_equal(u, 0) 
-   assert_almost_equal(v, -100) 
+    """Test the wind component."""
+    u, v = get_wind_components(100, 0)
+    assert_almost_equal(u, 0)
+    assert_almost_equal(v, -100)
 
-   speeds = np.array([100, 100, 100, 100])
-   directions = np.array([0, 90, 180, 275])
+    speeds = np.array([100, 100, 100, 100])
+    directions = np.array([0, 90, 180, 275])
+    u_expected = np.array([-0.000000e+00, -1.000000e+02, -1.224647e-14, 9.961947e+01])
+    u, v = get_wind_components(speeds, directions)
+    assert_array_almost_equal(u, u_expected)
 
-   u_expected = np.array([-0.000000e+00, -1.000000e+02, -1.224647e-14,  9.961947e+01])
-   v_expected = np.array([0,0,0,0])
 
-   u,v = get_wind_components(speeds, directions) 
-   assert_array_almost_equal(u, u_expected) 
+def test_value_error():
+    """Test value error."""
+    with pytest.raises(ValueError):
+        snell_angle(0, 0, 0)
 
-def test_value_error(): 
-   """ Test value error """ 
-   with pytest.raises(ValueError):
-       snell_angle(0, 0, 0)
 
 def test_warning_error():
-    """ Test warning error """ 
-    with pytest.warns(UserWarning): 
+    """Test warning error."""
+    with pytest.warns(UserWarning):
         get_wind_components(100, 400)
